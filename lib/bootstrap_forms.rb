cd45ff023d9a1e1f4ab651b2d007c153b116bed0
Igor Bochkariov <ujifgc@gmail.com>
2016-05-07 11:52:12 +0400

    compatibility with Padrino 0.12 and 0.13

--- lib/bootstrap_forms.rb
+++ lib/bootstrap_forms.rb
@@ -1,4 +1,6 @@
 require 'active_support'
+require 'active_support/core_ext/string/inflections'
+require 'active_support/core_ext/module/delegation'
 
 module BootstrapForms
   extend ActiveSupport::Autoload
